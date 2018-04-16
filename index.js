16572d49da9bfb86c7f6170ead3985f25d2528b2
Elango Ramanathan <elangoramanathan@Elangos-MacBook-Pro.local>
2018-04-15 18:25:02 -0700

    after adding console

--- index.js
+++ index.js
@@ -33,6 +33,7 @@ if ((process.env.NODE_ENV = "production")) {
   // Express will serve up the index.html file
   // if it doesn't recognize the route
   const path = require("path");
+  console.log(__dirname);
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
