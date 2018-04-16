7d0a7da1e19348d970d896deaac54213e9024864
Elango Ramanathan <elangoramanathan@Elangos-MacBook-Pro.local>
2018-04-15 19:03:52 -0700

    after adding console

--- index.js
+++ index.js
@@ -32,7 +32,6 @@ if ((process.env.NODE_ENV = "production")) {
   app.use(express.static(path.join(__dirname, "../client/build")));
   // Express will serve up the index.html file
   // if it doesn't recognize the route
-  const path = require("path");
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
     //res.sendFile(path.resolve("build", "index.html"));
