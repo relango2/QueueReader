60c69d58482febfba42733d7f3c85493b6aaf260
Elango Ramanathan <elangoramanathan@Elangos-MacBook-Pro.local>
2018-04-15 18:53:41 -0700

    after adding console

--- Config/keys.js
+++ Config/keys.js
@@ -1,5 +1,5 @@
 //Keys.js - place where it is decided to use the correcr set of Keys
-console.log(process.env.NODE_ENV);
+//console.log(process.env.NODE_ENV);
 if (process.env.NODE_ENV === "production") {
   //return production set of keys
   module.exports = require("./prod.js");
