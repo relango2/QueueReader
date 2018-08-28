cf5ddca84be23cbfd0c4078a7961ef7883e3405e
relango2 <relango2@gmail.com>
2018-08-27 18:19:24 -0700

    after fixing survey view

--- client/src/components/surveys/formFields.js
+++ client/src/components/surveys/formFields.js
@@ -1,6 +1,5 @@
 export default [
-  { label: "Campaign Title", name: "title" },
-  { label: "Subject Line", name: "subject" },
-  { label: "Email Body", name: "body" },
-  { label: "Recipient List", name: "recipients" }
+  { label: "Survey Topic", name: "title" },
+  { label: "Key", name: "subject" },
+  { label: "Value", name: "body" }
 ];
